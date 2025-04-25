36c58a07d4c3daea63e7de8ca0e9bbd252099bdc
adam reyes <adam.c.reyes@gmail.com>
2025-04-24 21:05:42 -0700

    add ruff python format/lint checks

--- tests/test_suites/isentropic_vortex/isentropic_vortex.py
+++ tests/test_suites/isentropic_vortex/isentropic_vortex.py
@@ -1,13 +1,8 @@
 # Modules
-import math
-import matplotlib
 import numpy as np
 from pathlib import Path
 
-matplotlib.use("agg")
-import matplotlib.pylab as plt
 import sys
-import os
 import utils.test_case
 
 """ To prevent littering up imported folders with .pyc files or __pycache_ folder"""
@@ -19,21 +14,21 @@ resolutions = []
 
 class TestCase(utils.test_case.TestCaseAbs):
     def Prepare(self, parameters, step):
-        mx = base_resolution * 2 ** step
+        mx = base_resolution * 2**step
         resolutions.append(mx)
         recon = "ppm"
         integrator = "rk2"
         parameters.driver_cmd_line_args = [
-                f"parthenon/job/problem_id=isentropic_vortex_{mx}",
-                f"parthenon/mesh/nx1={mx}",
-                f"parthenon/mesh/nx2={mx}",
-                f"parthenon/meshblock/nx1={mx / 2}",
-                f"parthenon/meshblock/nx2={mx / 2}",
-                f"parthenon/mesh/nghost=3",
-                f"parthenon/time/integrator={integrator}",
-                f"hydro/reconstruction={recon}",
-                f"parthenon/output0/file_type=hst",
-                f"parthenon/output0/dt=1.0",
+            f"parthenon/job/problem_id=isentropic_vortex_{mx}",
+            f"parthenon/mesh/nx1={mx}",
+            f"parthenon/mesh/nx2={mx}",
+            f"parthenon/meshblock/nx1={mx / 2}",
+            f"parthenon/meshblock/nx2={mx / 2}",
+            "parthenon/mesh/nghost=3",
+            f"parthenon/time/integrator={integrator}",
+            f"hydro/reconstruction={recon}",
+            "parthenon/output0/file_type=hst",
+            "parthenon/output0/dt=1.0",
         ]
         return parameters
 
@@ -46,10 +41,14 @@ class TestCase(utils.test_case.TestCaseAbs):
             errors.append(data[-1])
 
         with open("isentropic_vortex_convergence.out", "w") as fid:
-            min_slope = 100.
+            min_slope = 100.0
             for i in range(1, len(errors)):
-                slope = - np.log(errors[i]/errors[i-1]) / np.log(resolutions[i]/resolutions[i-1])
+                slope = -np.log(errors[i] / errors[i - 1]) / np.log(
+                    resolutions[i] / resolutions[i - 1]
+                )
                 min_slope = min(slope, min_slope)
-                fid.write(f"{resolutions[i-1]} {errors[i-1]} {resolutions[i]} {errors[i]} {slope}\n")
+                fid.write(
+                    f"{resolutions[i - 1]} {errors[i - 1]} {resolutions[i]} {errors[i]} {slope}\n"
+                )
 
         return min_slope > 1.5
