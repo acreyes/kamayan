d738fe1201be0819aa4df76f2a2808e3dde49932
adam reyes <adam.c.reyes@gmail.com>
2025-08-10 19:57:02 -0700

    physics

--- src/physics/physics.cpp
+++ src/physics/physics.cpp
@@ -2,34 +2,24 @@
 
 #include <memory>
 #include <string>
-#include <utility>
 
-#include "kamayan/config.hpp"
 #include "kamayan/runtime_parameters.hpp"
 #include "kamayan/unit.hpp"
+#include "kamayan/unit_data.hpp"
 #include "physics/physics_types.hpp"
 
 namespace kamayan::physics {
 std::shared_ptr<KamayanUnit> ProcessUnit() {
   auto physics = std::make_shared<KamayanUnit>("physics");
-  physics->Setup = Setup;
+  physics->SetupParams = SetupParams;
   return physics;
 }
 
-namespace rp = runtime_parameters;
-void Setup(Config *cfg, rp::RuntimeParameters *rps) {
-  auto fluid_str = rps->GetOrAdd<std::string>(
-      "physics", "fluid", "1T", "physics model to use for our fluid", {"1t", "3t"});
+void SetupParams(UnitDataCollection &udc) {
+  auto &physics = udc.AddData("physics");
+  physics.AddParm<Fluid>("fluid", "1T", "physics model to use for our fluid",
+                         {{"1t", Fluid::oneT}, {"3t", Fluid::threeT}});
 
-  auto fluid_type = MapStrToEnum<Fluid>(fluid_str, std::make_pair(Fluid::oneT, "1t"),
-                                        std::make_pair(Fluid::threeT, "3t"));
-  cfg->Add(fluid_type);
-
-  auto mhd_str =
-      rps->GetOrAdd<std::string>("physics", "MHD", "off", "Mhd model", {"off", "ct"});
-
-  auto Mhd_type = MapStrToEnum<Mhd>(mhd_str, std::make_pair(Mhd::off, "off"),
-                                    std::make_pair(Mhd::ct, "ct"));
-  cfg->Add(Mhd_type);
+  physics.AddParm<Mhd>("MHD", "off", "Mhd model", {{"off", Mhd::off}, {"ct", Mhd::ct}});
 }
 }  // namespace kamayan::physics
