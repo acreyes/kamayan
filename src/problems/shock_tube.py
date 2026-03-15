ef4aaae7dc6abcdb7ffe631fdc8e6b9688dd126f
adam reyes <adam.c.reyes@gmail.com>
2026-03-15 16:13:23 -0700

    I'm getting confused

--- src/problems/shock_tube.py
+++ src/problems/shock_tube.py
@@ -1,7 +1,7 @@
 """Problem setup for a shock tube with initial left/right states."""
 
 from dataclasses import dataclass
-from typing import Callable, Literal
+from typing import Callable, Literal, Sequence
 import numpy as np
 import typer
 
@@ -9,7 +9,7 @@ from kamayan.cli.app import kamayan_app
 from kamayan.code_units import Grid, driver, eos, physics
 from kamayan.code_units.Grid import AdaptiveGrid
 import kamayan.pyKamayan as pyKamayan
-from kamayan.pyKamayan.Grid import TopologicalElement as te
+from kamayan.pyKamayan.Grid import Coordinates_t, TopologicalElement as te
 from kamayan.code_units.Hydro import Hydro
 import kamayan.kamayan_manager as kman
 from kamayan.kamayan_manager import KamayanManager
@@ -18,9 +18,22 @@ from kamayan.pyKamayan.Grid import GetConfig
 from kamayan.pyKamayan.Options import Mhd
 
 _PROBLEMS = Literal["sod", "briowu", "einfeldt"]
-_DIMENSION = Literal[1, 2, 3]
+_DIMENSION = Literal[1, 2]  # support work out in 3d
 
 mhd_problems: list[_PROBLEMS] = ["briowu"]
+
+
+def _get_unit_dirs(
+    ndim: float, aspect12: int, aspect13: int, normalize: bool = False
+) -> tuple[np.ndarray, np.ndarray, np.ndarray]:
+    a12 = 1.0 / (float(aspect12) if ndim > 1 else np.inf)
+    a13 = 1.0 / (float(aspect13) if ndim > 2 else np.inf)
+    denom = np.sqrt(1 + a12**2) if normalize else 1.0
+
+    n_perp = np.array([1.0, a12, 0.0]) / denom
+    n_par1 = np.array([-a12, 1.0, 0.0]) / denom
+    n_par2 = [0.0, 0.0, 1.0]
+    return np.array(n_perp), np.array(n_par1), np.array(n_par2)
 
 
 @dataclass
@@ -88,17 +101,31 @@ def initialize(
         unit.AddParam("apsect13", aspect13)
 
         shock_tube = unit.Data("shock_tube")
+        n_perp, n_par1, n_par2 = _get_unit_dirs(
+            ndim, aspect12, aspect13, normalize=True
+        )
 
         def _get_state(s: Literal["L", "R"]) -> State:
+            # rotate states from the reference of the interface
+            vel_perp = shock_tube.Get(float, f"vel1{s}")
+            vel_par1 = shock_tube.Get(float, f"vel2{s}")
+            vel_par2 = shock_tube.Get(float, f"vel3{s}")
+            mag_perp = shock_tube.Get(float, f"mag1{s}")
+            mag_par1 = shock_tube.Get(float, f"mag2{s}")
+            mag_par2 = shock_tube.Get(float, f"mag3{s}")
+
             return State(
                 dens=shock_tube.Get(float, f"dens{s}"),
-                vel1=shock_tube.Get(float, f"vel1{s}"),
-                vel2=shock_tube.Get(float, f"vel3{s}"),
-                vel3=shock_tube.Get(float, f"vel3{s}"),
+                vel1=vel_perp * n_perp[0] + vel_par1 * n_par1[0] + vel_par2 * n_par2[0],
+                vel2=vel_perp * n_perp[1] + vel_par1 * n_par1[1] + vel_par2 * n_par2[1],
+                vel3=vel_perp * n_perp[2] + vel_par1 * n_par1[2] + vel_par2 * n_par2[2],
                 pres=shock_tube.Get(float, f"pres{s}"),
-                mag1=shock_tube.Get(float, f"mag1{s}"),
-                mag2=shock_tube.Get(float, f"mag2{s}"),
-                mag3=shock_tube.Get(float, f"mag3{s}"),
+                mag1=mag_perp,
+                mag2=mag_par1,
+                mag3=mag_par2,
+                # mag1=mag_perp * n_perp[0] + mag_par1 * n_par1[0] + mag_par2 * n_par2[0],
+                # mag2=mag_perp * n_perp[1] + mag_par1 * n_par1[1] + mag_par2 * n_par2[1],
+                # mag3=mag_perp * n_perp[2] + mag_par1 * n_par1[2] + mag_par2 * n_par2[2],
             )
 
         unit.AddParam("vL", _get_state("L"))
@@ -107,11 +134,86 @@ def initialize(
     return _initialize
 
 
+def _get_coords(
+    indices: np.ndarray, coords: Coordinates_t, el: te = te.CC
+) -> tuple[np.ndarray, np.ndarray, np.ndarray]:
+    xx = np.array(coords.Xc1(indices[2, :, :, :]))
+    yy = np.array(coords.Xc2(indices[1, :, :, :]))
+    zz = np.array(coords.Xc2(indices[0, :, :, :]))
+    if el == te.F1:
+        xx = np.array(coords.Xf1(indices[2, :, :, :]))
+    elif el == te.F2:
+        yy = np.array(coords.Xf2(indices[1, :, :, :]))
+    elif el == te.F3:
+        zz = np.array(coords.Xf3(indices[0, :, :, :]))
+
+    return xx, yy, zz
+
+
+def _x_perp(
+    n_perp: np.ndarray,
+    xx: np.ndarray,
+    yy: np.ndarray,
+    zz: np.ndarray,
+    wrap: bool = True,
+) -> np.ndarray:
+    x_perp = n_perp[0] * xx + n_perp[1] * yy + n_perp[2] * zz
+    if wrap:
+        return x_perp % 1.0
+    return x_perp
+
+
+def _calc_face_mag(
+    face: np.ndarray,
+    el: te,
+    coords: Coordinates_t,
+    n_perp: np.ndarray,
+    n_par1: np.ndarray,
+    vL: State,
+    vR: State,
+) -> np.ndarray:
+    if el not in [te.F1, te.F2, te.F3]:
+        raise ValueError(f"Topological Element {el} not a face")
+    # coordinates at the face el
+    xx, yy, zz = _get_coords(np.indices(face.shape), coords, el)
+    # shift by delta / 2 to the edges
+    if el == te.F1:
+        yy -= 0.5 * coords.Dx2()
+    if el == te.F2:
+        xx -= 0.5 * coords.Dx1()
+
+    def Az(x: np.ndarray, y: np.ndarray, z: np.ndarray) -> np.ndarray:
+        x_perp = _x_perp(n_perp, x, y, z)
+        x_par1 = _x_perp(n_par1, x, y, z, wrap=False)
+        ## we need for Az to be continuous at x_per == 0.5
+        return np.where(
+            x_perp <= 0.5,
+            (x_perp - 0.5) * vL.mag2 - x_par1 * vL.mag1 * 0.0,
+            (x_perp - 0.5) * vR.mag2 - x_par1 * vR.mag1 * 0.0,
+        )
+
+    A = Az(xx, yy, zz)
+    if el == te.F1:
+        # return (np.roll(A, 1, axis=1) - A) / coords.Dx2()
+        return (Az(xx, yy + coords.Dx2(), zz) - Az(xx, yy, zz)) / coords.Dx2()
+    elif el == te.F2:
+        # return -(np.roll(A, 1, axis=2) - A) / coords.Dx1()
+        return -(Az(xx + coords.Dx1(), yy, zz) - Az(xx, yy, zz)) / coords.Dx1()
+
+    return face
+
+
 def pgen(mb: pyKamayan.Grid.MeshBlock):
     """Single block initial conditions for shock tube."""
     shock_tube = mb.get_package("shock_tube")
     vL = shock_tube.GetParam(State, "vL")
     vR = shock_tube.GetParam(State, "vR")
+
+    ndim = shock_tube.GetParam(int, "ndim")
+    aspect12 = shock_tube.GetParam(int, "apsect12")
+    aspect13 = shock_tube.GetParam(int, "apsect13")
+
+    n_perp, n_par1, n_par2 = _get_unit_dirs(ndim, aspect12, aspect13, normalize=True)
 
     config = GetConfig(mb)
     mhd = config.GetMhd()
@@ -128,18 +230,22 @@ def pgen(mb: pyKamayan.Grid.MeshBlock):
     vel2 = np.array(pack.GetParArray3D(0, "velocity", te.CC, 1).view(), copy=False)
     vel3 = np.array(pack.GetParArray3D(0, "velocity", te.CC, 2).view(), copy=False)
 
-    indices = np.indices(dens.shape)
-    xx = np.array(coords.Xc1(indices[2, :, :, :]))
-    yy = np.array(coords.Xc2(indices[1, :, :, :]))
+    xx, yy, zz = _get_coords(np.indices(dens.shape), coords)
 
-    dens[:, :, :] = np.where(xx <= 0.5, vL.dens, vR.dens)
-    vel1[:, :, :] = np.where(xx <= 0.5, vL.vel1, vR.vel1)
-    vel2[:, :, :] = np.where(xx <= 0.5, vL.vel2, vR.vel2)
-    vel3[:, :, :] = np.where(xx <= 0.5, vL.vel3, vR.vel3)
-    pres[:, :, :] = np.where(xx <= 0.5, vL.pres, vR.pres)
+    x_perp = _x_perp(n_perp, xx, yy, zz)
+
+    dens[:, :, :] = np.where(x_perp <= 0.5, vL.dens, vR.dens)
+    vel1[:, :, :] = np.where(x_perp <= 0.5, vL.vel1, vR.vel1)
+    vel2[:, :, :] = np.where(x_perp <= 0.5, vL.vel2, vR.vel2)
+    vel3[:, :, :] = np.where(x_perp <= 0.5, vL.vel3, vR.vel3)
+    pres[:, :, :] = np.where(x_perp <= 0.5, vL.pres, vR.pres)
 
     if ct:
+        # B = curl(A)
+        # Bx = dyAz     By = -dxAz
+        # A = Bx*y - By*x
         fac1 = np.array(pack.GetParArray3D(0, "mag", te.F1).view(), copy=False)
+        fac2 = np.array(pack.GetParArray3D(0, "mag", te.F2).view(), copy=False)
         mag1 = np.array(pack.GetParArray3D(0, "magc", te.CC, 0).view(), copy=False)
         mag2 = np.array(pack.GetParArray3D(0, "magc", te.CC, 1).view(), copy=False)
         mag3 = np.array(pack.GetParArray3D(0, "magc", te.CC, 2).view(), copy=False)
@@ -147,6 +253,10 @@ def pgen(mb: pyKamayan.Grid.MeshBlock):
         fac1[:, :, :] = vL.mag1
         mag2[:, :, :] = np.where(xx <= 0.5, vL.mag2, vR.mag2)
         mag3[:, :, :] = np.where(xx <= 0.5, vL.mag3, vR.mag3)
+
+        if ndim > 1:
+            fac1[:, :, :] = _calc_face_mag(fac1, te.F1, coords, n_perp, n_par1, vL, vR)
+            fac2[:, :, :] = _calc_face_mag(fac2, te.F2, coords, n_perp, n_par1, vL, vR)
 
 
 @kamayan_app(description="Shock Tube with initial left/right states")
@@ -179,12 +289,14 @@ def shock_tube(
     )
     km = KamayanManager("shock_tube", units)
 
-    nxb = 128
+    nxb = 32
     nblocks = int(128 / nxb)
+    mult = 2.0 if ndim > 1 else 1.0
+    L = 1.0 / np.sqrt(1 + aspect12**2) if ndim > 1 else 1.0
     km.grid = AdaptiveGrid(
-        xbnd1=(0.0, 1.0),
-        xbnd2=(0.0, 1.0 * aspect12),
-        xbnd3=(0.0, 1.0 * aspect13),
+        xbnd1=(0.0, L * mult),
+        xbnd2=(0.0, L * mult * aspect12),
+        xbnd3=(0.0, L * mult * aspect13),
         nxb1=nxb,
         nxb2=nxb if ndim > 1 else 1,
         nxb3=nxb if ndim > 2 else 1,
