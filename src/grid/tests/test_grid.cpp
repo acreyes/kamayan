9b54e132a254920b1ec38a0333271391cd9aa01e
adam reyes <adam.c.reyes@gmail.com>
2026-03-31 00:37:45 -0700

    fix unit tests

--- src/grid/tests/test_grid.cpp
+++ src/grid/tests/test_grid.cpp
@@ -7,11 +7,14 @@
 #include <mesh/meshblock.hpp>
 
 #include "basic_types.hpp"
+#include "grid/geometry_types.hpp"
 #include "grid/grid.hpp"
 #include "grid/grid_types.hpp"
 #include "grid/scratch_variables.hpp"
 #include "grid/subpack.hpp"
+#include "kamayan/config.hpp"
 #include "kamayan/fields.hpp"
+#include "kamayan/runtime_parameters.hpp"
 #include "kamayan/unit.hpp"
 #include "kamayan_utils/parallel.hpp"
 #include "kokkos_abstraction.hpp"
@@ -47,6 +50,13 @@ TEST(grid, PackIndexer) {
   constexpr int NXB = 8;
   constexpr int NBLOCKS = 9;
   auto pkg = std::make_shared<KamayanUnit>("Test Package");
+
+  // need to add a config to the unit for AddFields
+  auto rps = std::make_shared<runtime_parameters::RuntimeParameters>();
+  auto cfg = std::make_shared<Config>();
+  cfg->Add(Geometry::cartesian);
+  pkg->InitResources(rps, cfg);
+
   AddFields(Fields(), pkg.get(), {CENTER_FLAGS(Metadata::WithFluxes)});
 
   // now build our test grid
