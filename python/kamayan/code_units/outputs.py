97e3871deadd76bb089b42d0587e2ea56345f458
Henry Collis <henry.collis@pacificfusion.com>
2026-04-16 21:00:05 +0000

    adding variables to output

--- python/kamayan/code_units/outputs.py
+++ python/kamayan/code_units/outputs.py
@@ -15,12 +15,14 @@ class OutputType:
     file_type: str
     dt: Optional[float] = None
     dn: Optional[int] = None
+    variables: Optional[str] = None
 
     def set_params(self, n_out: int, params: KamayanParams):
         """Set our input parameters."""
         self.dt = self.dt if self.dt else -1.0
         self.dn = self.dn if self.dn else -1
-        params[f"parthenon/output{n_out}"] = asdict(self)
+        d = {k: v for k, v in asdict(self).items() if v is not None}
+        params[f"parthenon/output{n_out}"] = d
 
 
 class KamayanOutputs(Node):
@@ -36,13 +38,20 @@ class KamayanOutputs(Node):
         self._outputs.clear()
 
     def add(
-        self, name, file_type: str, dt: Optional[float] = None, dn: Optional[int] = None
+        self,
+        name,
+        file_type: str,
+        dt: Optional[float] = None,
+        dn: Optional[int] = None,
+        variables: Optional[str] = None,
     ):
         """Add a new output."""
         if dt and dn:
             raise ValueError("Can only provide one of dt or dn for a given output")
 
-        self._outputs[name] = OutputType(name=name, file_type=file_type, dt=dt, dn=dn)
+        self._outputs[name] = OutputType(
+            name=name, file_type=file_type, dt=dt, dn=dn, variables=variables
+        )
 
     def set_params(self, params: KamayanParams):
         """Set inputs for all outputs."""
